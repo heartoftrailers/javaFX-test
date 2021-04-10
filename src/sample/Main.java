203b126b2881ddc0131e862f06c587b897f1155b
heartoftrailers <ciovicaflorin0@gmail.com>
2021-04-10 22:55:15 +0100

    Customize users (add picture profile) not finished but works

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
-//        Parent root = FXMLLoader.load(getClass().getResource("view/additem.fxml"));
+//        Parent root = FXMLLoader.load(getClass().getResource("view/login.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("view/list.fxml"));
         primaryStage.setTitle("ToDo");
         primaryStage.setScene(new Scene(root, 700, 400));
         primaryStage.show();
